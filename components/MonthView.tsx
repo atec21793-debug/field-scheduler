1422ba14cb5fc870d6dc4fe084c9a2b7148b5c45
atec21793-debug <atec21793@gmail.com>
2026-08-03 22:54:53 +0900

    a

--- components/MonthView.tsx
+++ components/MonthView.tsx
@@ -53,10 +53,10 @@ export default function MonthView({ currentDate, events, onSelectEvent, onCellCl
                   {date.getDate()}
                 </span>
               </div>
-              {/* 各カードをブロック要素として強制的に1行に収め、はみ出た分を省略する */}
+              {/* カード全体を強制的に1行に切り詰め、はみ出した部分を省略記号（…）にする */}
               <div className="flex-1 space-y-0.5 overflow-hidden">
                 {dayEvents.map((event) => (
-                  <div key={event.id} className="w-full overflow-hidden whitespace-nowrap text-ellipsis text-[10px] leading-tight">
+                  <div key={event.id} className="w-full overflow-hidden whitespace-nowrap text-ellipsis text-[10px] leading-tight block [&_*]:inline">
                     <EventCard event={event} onClick={() => onSelectEvent(event)} />
                   </div>
                 ))}
