7318e07b3fa7c4ccb2b973152394facb6a7d6e59
atec21793-debug <atec21793@gmail.com>
2026-08-07 13:04:59 +0900

    ｌｌｌ

--- app/un-ordered/page.tsx
+++ app/un-ordered/page.tsx
@@ -22,6 +22,7 @@ export default function UnOrderedListPage() {
       .not('title', 'ilike', '%(く)%')
       .not('title', 'ilike', '%（工事）%')
       .not('title', 'ilike', '%(工事)%')
+      .not('title', 'ilike', '%(現調)%')
       .order('date', { ascending: true });
 
     const { data, error } = await query;
